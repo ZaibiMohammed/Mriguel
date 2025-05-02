aa4cdf691eff5320cc267715815385fc329d41e6
ZaibiMed <mohammed.zaibi@outlook.com>
2025-05-02 21:54:25 +0200

    Add IApplicationDbContext interface

---
+++ src/Core/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Mriguel.Domain.Entities;
+
+namespace Mriguel.Application.Common.Interfaces
+{
+    /// <summary>
+    /// Interface for the application database context
+    /// </summary>
+    public interface IApplicationDbContext
+    {
+        DbSet<User> Users { get; }
+        DbSet<Item> Items { get; }
+        DbSet<Rental> Rentals { get; }
+        DbSet<ItemCategory> ItemCategories { get; }
+        DbSet<ItemImage> ItemImages { get; }
+        DbSet<ItemAvailability> ItemAvailabilities { get; }
+        DbSet<Review> Reviews { get; }
+        DbSet<Payment> Payments { get; }
+        DbSet<Address> Addresses { get; }
+        DbSet<UserVerification> UserVerifications { get; }
+        DbSet<PaymentMethod> PaymentMethods { get; }
+        DbSet<RentalMessage> RentalMessages { get; }
+        
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
