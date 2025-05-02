ab7b1397a4c007b6fcbbcd56831a0ee8148c7908
ZaibiMed <mohammed.zaibi@outlook.com>
2025-05-03 00:20:01 +0200

    Add IEmailService interface

--- src/Core/Application/Common/Interfaces/IEmailService.cs
+++ src/Core/Application/Common/Interfaces/IEmailService.cs
@@ -1,29 +1,39 @@
-using System.Threading.Tasks;
-
 namespace Mriguel.Application.Common.Interfaces
 {
     /// <summary>
-    /// Email service interface
+    /// Interface for email service operations
     /// </summary>
     public interface IEmailService
     {
         /// <summary>
         /// Sends an email
         /// </summary>
-        /// <param name="to">Recipient email address</param>
-        /// <param name="subject">Email subject</param>
-        /// <param name="htmlContent">Email content in HTML format</param>
-        /// <param name="textContent">Email content in plain text format (optional)</param>
-        /// <returns>True if the email was sent successfully</returns>
         Task<bool> SendEmailAsync(string to, string subject, string htmlContent, string textContent = null);
         
         /// <summary>
-        /// Sends a templated email
+        /// Sends an email with attachments
         /// </summary>
-        /// <param name="to">Recipient email address</param>
-        /// <param name="templateId">Template ID</param>
-        /// <param name="templateData">Template data</param>
-        /// <returns>True if the email was sent successfully</returns>
-        Task<bool> SendTemplatedEmailAsync(string to, string templateId, object templateData);
+        Task<bool> SendEmailWithAttachmentsAsync(string to, string subject, string htmlContent, string textContent = null, IEnumerable<EmailAttachment> attachments = null);
+    }
+    
+    /// <summary>
+    /// Represents an email attachment
+    /// </summary>
+    public class EmailAttachment
+    {
+        /// <summary>
+        /// Name of the attachment
+        /// </summary>
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// Content of the attachment as a byte array
+        /// </summary>
+        public byte[] Content { get; set; }
+        
+        /// <summary>
+        /// MIME type of the attachment
+        /// </summary>
+        public string ContentType { get; set; }
     }
 }
