7d4f2379d74f24fa2e1b1e02e0d6042f52d0ad25
ZaibiMed <mohammed.zaibi@outlook.com>
2025-05-03 00:20:54 +0200

    Add PaginatedList model

--- src/Core/Application/Common/Models/PaginatedList.cs
+++ src/Core/Application/Common/Models/PaginatedList.cs
@@ -1,27 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace Mriguel.Application.Common.Models
 {
     /// <summary>
-    /// Paginated list for collections
+    /// Represents a paginated list of items
     /// </summary>
-    /// <typeparam name="T">Type of the items</typeparam>
     public class PaginatedList<T>
     {
         /// <summary>
-        /// Current page items
+        /// Items in the current page
         /// </summary>
         public List<T> Items { get; }
         
         /// <summary>
-        /// Current page number (1-based)
+        /// Current page index
         /// </summary>
-        public int PageNumber { get; }
+        public int PageIndex { get; }
         
         /// <summary>
         /// Total number of pages
@@ -32,54 +26,37 @@ namespace Mriguel.Application.Common.Models
         /// Total number of items
         /// </summary>
         public int TotalCount { get; }
-        
+
         /// <summary>
-        /// Whether there is a previous page
+        /// Creates a new paginated list
         /// </summary>
-        public bool HasPreviousPage => PageNumber > 1;
-        
-        /// <summary>
-        /// Whether there is a next page
-        /// </summary>
-        public bool HasNextPage => PageNumber < TotalPages;
-        
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        public PaginatedList(List<T> items, int count, int pageNumber, int pageSize)
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
         {
-            PageNumber = pageNumber;
+            PageIndex = pageIndex;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
             TotalCount = count;
             Items = items;
         }
-        
+
         /// <summary>
-        /// Creates a paginated list from a collection
+        /// Indicates whether there is a previous page
         /// </summary>
-        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize, CancellationToken cancellationToken = default)
-        {
-            var count = await source.CountAsync(cancellationToken);
-            
-            pageNumber = pageNumber < 1 ? 1 : pageNumber;
-            pageSize = pageSize < 1 ? 10 : pageSize;
-            
-            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync(cancellationToken);
-            
-            return new PaginatedList<T>(items, count, pageNumber, pageSize);
-        }
-        
+        public bool HasPreviousPage => PageIndex > 1;
+
         /// <summary>
-        /// Creates a paginated list from a collection
+        /// Indicates whether there is a next page
         /// </summary>
-        public static PaginatedList<T> Create(IEnumerable<T> source, int count, int pageNumber, int pageSize)
+        public bool HasNextPage => PageIndex < TotalPages;
+
+        /// <summary>
+        /// Creates a paginated list from a query
+        /// </summary>
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
         {
-            pageNumber = pageNumber < 1 ? 1 : pageNumber;
-            pageSize = pageSize < 1 ? 10 : pageSize;
-            
-            var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
-            
-            return new PaginatedList<T>(items, count, pageNumber, pageSize);
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
         }
     }
 }
