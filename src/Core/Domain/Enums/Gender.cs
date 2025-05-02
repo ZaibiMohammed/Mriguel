5cde78de3b75ee57754a82e34eda42a0c05c864e
ZaibiMed <mohammed.zaibi@outlook.com>
2025-05-02 21:49:37 +0200

    Add Gender enum

---
+++ src/Core/Domain/Enums/Gender.cs
@@ -0,0 +1,33 @@
+namespace Mriguel.Domain.Enums
+{
+    /// <summary>
+    /// Represents a gender
+    /// </summary>
+    public enum Gender
+    {
+        /// <summary>
+        /// Male gender
+        /// </summary>
+        Male = 1,
+        
+        /// <summary>
+        /// Female gender
+        /// </summary>
+        Female = 2,
+        
+        /// <summary>
+        /// Non-binary gender
+        /// </summary>
+        NonBinary = 3,
+        
+        /// <summary>
+        /// Prefer not to say
+        /// </summary>
+        PreferNotToSay = 4,
+        
+        /// <summary>
+        /// Other gender identity
+        /// </summary>
+        Other = 5
+    }
+}
